9036a87e89d31d3aa03a584807f2acbdc83e92c0
BenGutteridge <benpgutteridge@gmail.com>
2022-10-24 18:04:51 +0100

    bash script for training multiple files

---
+++ bash_scripts/progress_bar.py
@@ -0,0 +1,31 @@
+# a script to serve as a progress bar when running a bash script
+
+import os
+import os.path as osp
+import sys
+import time
+
+"""
+we want to put this between cfg runs on a bash script
+
+takes the name of the run (which contains model and dataset info)
+
+literally just has to add the name of it to a text doc
+"""
+ 
+# total arguments
+args = len(sys.argv)
+
+filename = '../results/progress.txt'
+
+try: 
+  run_name = args[1]
+  timestr = time.strftime("%m%d-%H%M%S")
+  message = ["%s COMPLETED AT %S" % (run_name, timestr)]
+  with open(filename, 'a') as f:
+    f.write('\n'.join(message))
+except:
+  if osp.exists(filename):
+    os.remove(filename)
+  with open(filename, 'w') as f:
+    f.write('PROGRESS')
---
+++ bash_scripts/run_scripts_verges.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+python progress_bar.py
+cd ..
+models=( 
+  GCN
+  DelayGCN
+)
+datasets=(
+  peptides-func
+)
+for model in "${models[@]}" ; do
+  for dataset in "${datasets[@]}" ; do
+    python main.py --cfg "configs/$model/$dataset-$model.yaml" --device cuda
+    python bash_scripts/progress_bar.py "$model-$dataset"
+  done
+done
