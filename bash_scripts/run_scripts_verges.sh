671a6909272aab824f20d9bdc0e707c9ca0e7f2c
BenGutteridge <benpgutteridge@gmail.com>
2022-10-24 23:16:24 +0100

    errors in bash scripts

--- bash_scripts/run_scripts_verges.sh
+++ bash_scripts/run_scripts_verges.sh
@@ -10,7 +10,7 @@ datasets=(
 )
 for model in "${models[@]}" ; do
   for dataset in "${datasets[@]}" ; do
-    python main.py --cfg "configs/$model/$dataset-$model.yaml" device cuda
+    python main.py --cfg "configs/$model/$dataset-$model.yaml" device cuda optim.max_epoch 1
     python bash_scripts/progress_bar.py "$model-$dataset"
   done
 done
