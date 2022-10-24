aa64388795fbffd35c0e875fcff86f4af60f66ec
BenGutteridge <benpgutteridge@gmail.com>
2022-10-24 23:24:13 +0100

    bash script editing

--- bash_scripts/run_scripts_verges.sh
+++ bash_scripts/run_scripts_verges.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python progress_bar.py
 cd ..
+python progress_bar.py
 models=( 
   GCN
   DelayGCN
@@ -11,6 +11,6 @@ datasets=(
 for model in "${models[@]}" ; do
   for dataset in "${datasets[@]}" ; do
     python main.py --cfg "configs/$model/$dataset-$model.yaml" device cuda optim.max_epoch 1
-    # python bash_scripts/progress_bar.py "$model-$dataset"
+    python bash_scripts/progress_bar.py "$model-$dataset"
   done
 done
