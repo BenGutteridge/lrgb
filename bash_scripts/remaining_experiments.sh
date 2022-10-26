a8cbb770f5acf6ad9a3623ee9cd53cce054a2dce
BenGutteridge <benpgutteridge@gmail.com>
2022-10-26 23:56:20 +0100

    bash script

--- bash_scripts/remaining_experiments.sh
+++ bash_scripts/remaining_experiments.sh
@@ -2,24 +2,24 @@
 cd ..
 python bash_scripts/progress_bar.py
 runs=(
-  configs/GCNII/peptides-struct-GCNII.yaml
-  configs/GINE/peptides-struct-GINE.yaml
-  configs/GatedGCN/peptides-struct-GatedGCN.yaml
-  configs/MLP/peptides-struct-MLP.yaml
-  configs/SAN/peptides-struct-SAN+RWSE.yaml
-  configs/SAN/peptides-struct-SAN.yaml
-  configs/expts_l2/GCNII/peptides-struct-GCNII.yaml
-  configs/expts_l2/GINE/peptides-struct-GINE.yaml
-  configs/expts_l2/GatedGCN/peptides-struct-GatedGCN.yaml
-  configs/DeLiteGCN/pcqm-contact-DeLiteGCN.yaml
-  configs/GCNII/pcqm-contact-GCNII.yaml
-  configs/GINE/pcqm-contact-GINE.yaml
-  configs/GatedGCN/pcqm-contact-GatedGCN.yaml
-  configs/SAN/pcqm-contact-SAN.yaml
-  configs/SAN/pcqm-contact-SAN+RWSE.yaml
-  configs/expts_l2/GCNII/pcqm-contact-GCNII.yaml
-  configs/expts_l2/GINE/pcqm-contact-GINE.yaml
-  configs/expts_l2/GatedGCN/pcqm-contact-GatedGCN.yaml
+  # configs/GCNII/peptides-struct-GCNII.yaml
+  # configs/GINE/peptides-struct-GINE.yaml
+  # configs/GatedGCN/peptides-struct-GatedGCN.yaml
+  # configs/MLP/peptides-struct-MLP.yaml
+  # configs/SAN/peptides-struct-SAN+RWSE.yaml
+  # configs/SAN/peptides-struct-SAN.yaml
+  # configs/expts_l2/GCNII/peptides-struct-GCNII.yaml
+  # configs/expts_l2/GINE/peptides-struct-GINE.yaml
+  # configs/expts_l2/GatedGCN/peptides-struct-GatedGCN.yaml
+  # configs/DeLiteGCN/pcqm-contact-DeLiteGCN.yaml
+  # configs/GCNII/pcqm-contact-GCNII.yaml
+  # configs/GINE/pcqm-contact-GINE.yaml
+  # configs/GatedGCN/pcqm-contact-GatedGCN.yaml
+  # configs/SAN/pcqm-contact-SAN.yaml
+  # configs/SAN/pcqm-contact-SAN+RWSE.yaml
+  # configs/expts_l2/GCNII/pcqm-contact-GCNII.yaml
+  # configs/expts_l2/GINE/pcqm-contact-GINE.yaml
+  # configs/expts_l2/GatedGCN/pcqm-contact-GatedGCN.yaml
   configs/DeLiteGCN/vocsuperpixels-DeLiteGCN.yaml
   configs/DelayGCN/vocsuperpixels-DelayGCN.yaml
   configs/GCN/vocsuperpixels-GCN.yaml
