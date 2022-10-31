0a07e7813c508d336b9800a42de9ce155bf9036e
BenGutteridge <benpgutteridge@gmail.com>
2022-10-31 10:11:54 +0000

    exp bash

---
+++ bash_scripts/22.10.31_repeat_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ..
+python bash_scripts/progress_bar.py
+runs=(
+  configs/alpha_kGCN/peptides-func-alpha_kGCN.yaml
+  configs/KGCN/peptides-func-KGCN.yaml
+  configs/DelayGCN/peptides-func-DelayGCN.yaml
+  configs/GCN/peptides-func-GCN.yaml
+  # configs/SAN/peptides-func-SAN.yaml
+  # configs/SAN/peptides-func-SAN+RWSE.yaml
+)
+for run in "${runs[@]}" ; do
+  python main.py --cfg "$run" device cuda dataset.dir --repeat 3 /data/beng/datasets
+  python bash_scripts/progress_bar.py "$run"
+done
