755736427a7f9feaf16c42a4027d5da18eb4cbc0
BenGutteridge <benpgutteridge@gmail.com>
2022-11-02 17:33:16 +0000

    3 repeats - san struct and func

--- bash_scripts/22.10.31_repeat_3.sh
+++ bash_scripts/22.10.31_repeat_3.sh
@@ -2,14 +2,17 @@
 cd ..
 python bash_scripts/progress_bar.py
 runs=(
-  configs/alpha_kGCN/peptides-func-alpha_kGCN.yaml
-  configs/KGCN/peptides-func-KGCN.yaml
-  configs/DelayGCN/peptides-func-DelayGCN.yaml
-  configs/GCN/peptides-func-GCN.yaml
-  # configs/SAN/peptides-func-SAN.yaml
-  # configs/SAN/peptides-func-SAN+RWSE.yaml
+  # configs/alpha_kGCN/peptides-func-alpha_kGCN.yaml
+  # configs/KGCN/peptides-func-KGCN.yaml
+  # configs/DelayGCN/peptides-func-DelayGCN.yaml
+  # configs/GCN/peptides-func-GCN.yaml
+  configs/SAN/peptides-func-SAN.yaml
+  configs/SAN/peptides-func-SAN+RWSE.yaml
+  configs/SAN/peptides-struct-SAN.yaml
+  configs/SAN/peptides-struct-SAN+RWSE.yaml
+
 )
 for run in "${runs[@]}" ; do
-  python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets
+  python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size: 16
   python bash_scripts/progress_bar.py "$run"
 done
