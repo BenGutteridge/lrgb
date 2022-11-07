4b69d1fab9cdc21cd582ee41e69068cd258ddd2b
BenGutteridge <benpgutteridge@gmail.com>
2022-11-07 11:58:21 +0000

    rbar inf workaround using -1

--- graphgps/stage/utils.py
+++ graphgps/stage/utils.py
@@ -10,7 +10,10 @@ def init_khop_GCN(model, dim_in, dim_out, num_layers):
   """The k-hop GCN param initialiser, used for k_gnn and delay_gnn"""
   model.num_layers = num_layers
   model.max_k = cfg.gnn.layers_mp # cfg.delay.max_k
-  model.rbar = cfg.rbar # default 1
+  if model.rbar == -1: # can't set inf in cfg
+    model.rbar = float('inf')
+  else:
+    model.rbar = cfg.rbar # default 1
   for t in range(num_layers):
       d_in = dim_in if t == 0 else dim_out
       K = min(model.max_k, t+1)
