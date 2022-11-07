15b7ff860ba5bd9e2cf5e8892b13710cbe071869
BenGutteridge <benpgutteridge@gmail.com>
2022-11-07 13:45:13 +0000

    betaGCN stretched results

--- bash_scripts/22.11.07_betaGCN_stretched.sh
+++ bash_scripts/22.11.07_betaGCN_stretched.sh
@@ -21,6 +21,6 @@ configs/betaGCN/500k_stretched/peptides-func-betaGCN_L=15_d=175_beta=5.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 2 device cuda dataset.dir /data/beng/datasets optim.max_epoch 300
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets optim.max_epoch 300
   python bash_scripts/progress_bar.py "$run"
 done
