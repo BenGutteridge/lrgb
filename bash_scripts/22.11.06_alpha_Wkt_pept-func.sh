7d98c768447a8e46196bea634e43c904334dd783
BenGutteridge <benpgutteridge@gmail.com>
2022-11-07 09:46:13 +0000

    alpha_W_kt keyname

--- bash_scripts/22.11.06_alpha_Wkt_pept-func.sh
+++ bash_scripts/22.11.06_alpha_Wkt_pept-func.sh
@@ -10,6 +10,6 @@ runs=(
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets optim.max_epoch 300
   python bash_scripts/progress_bar.py "$run"
 done
