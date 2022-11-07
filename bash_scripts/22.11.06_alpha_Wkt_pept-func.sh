d4c102206f01ea3ecfaf83b4254b5eb1806ed4b5
BenGutteridge <benpgutteridge@gmail.com>
2022-11-07 09:46:32 +0000

    alpha_W_kt keyname

--- bash_scripts/22.11.06_alpha_Wkt_pept-func.sh
+++ bash_scripts/22.11.06_alpha_Wkt_pept-func.sh
@@ -2,7 +2,7 @@
 cd ..
 python bash_scripts/progress_bar.py
 runs=(
-  configs/KGCN/500k_stretched/peptides-func-KGCN_L=07_d=130_RUN_AGAIN.yaml
+  # configs/KGCN/500k_stretched/peptides-func-KGCN_L=07_d=130_RUN_AGAIN.yaml
   configs/alpha_kGCN/peptides-func-alpha_kt_GCN_L=05_d=175.yaml
   configs/alpha_kGCN/peptides-func-alpha_kt_GCN_L=07_d=130.yaml
   configs/alpha_kGCN/peptides-func-alpha_kt_GCN_L=09_d=100.yaml
