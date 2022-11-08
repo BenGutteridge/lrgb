0c16233066f56d2b84a03f0a9863978b3c2def58
BenGutteridge <benpgutteridge@gmail.com>
2022-11-08 10:37:00 +0000

    rerunning zinc runs

--- bash_scripts/22.11.07_ZINC_runs.sh
+++ bash_scripts/22.11.07_ZINC_runs.sh
@@ -2,27 +2,28 @@
 cd ..
 python bash_scripts/progress_bar.py
 runs=(
-# gcn
-configs/GCN/ZINC-GCN_L=02_d=500.yaml
-configs/GCN/ZINC-GCN_L=05_d=300.yaml
+# one of each, test
+configs/alphaGCN/ZINC-alphaGCN_L=05_d=140.yaml
+configs/betaGCN/ZINC-betaGCN_L=05_d=300_beta=2.yaml
+configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=01.yaml
+configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=inf.yaml
+# # gcn
+# configs/GCN/ZINC-GCN_L=02_d=500.yaml
+# configs/GCN/ZINC-GCN_L=05_d=300.yaml
 configs/GCN/ZINC-GCN_L=07_d=250.yaml
 # alpha
 configs/alphaGCN/ZINC-alphaGCN_L=02_d=350.yaml
-configs/alphaGCN/ZINC-alphaGCN_L=05_d=140.yaml
 configs/alphaGCN/ZINC-alphaGCN_L=07_d=100.yaml
 # beta
 configs/betaGCN/ZINC-betaGCN_L=02_d=500_beta=2.yaml
-configs/betaGCN/ZINC-betaGCN_L=05_d=300_beta=2.yaml
 configs/betaGCN/ZINC-betaGCN_L=05_d=300_beta=5.yaml
 configs/betaGCN/ZINC-betaGCN_L=07_d=250_beta=2.yaml
 configs/betaGCN/ZINC-betaGCN_L=07_d=250_beta=7.yaml
 # rbar 1 - inf, L=2-9
 configs/rbar-GCN/ZINC-rGCN_L=02_d=400_r=01.yaml
 configs/rbar-GCN/ZINC-rGCN_L=02_d=400_r=inf.yaml
-configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=01.yaml
 # configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=02.yaml
 # configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=04.yaml
-configs/rbar-GCN/ZINC-rGCN_L=05_d=175_r=inf.yaml
 configs/rbar-GCN/ZINC-rGCN_L=07_d=130_r=01.yaml
 # configs/rbar-GCN/ZINC-rGCN_L=07_d=130_r=02.yaml
 # configs/rbar-GCN/ZINC-rGCN_L=07_d=130_r=04.yaml
