660e2b12a952b45c07bc07fabd995286e073c9fa
BenGutteridge <benpgutteridge@gmail.com>
2022-11-08 09:41:32 +0000

    finishing stopped run

--- bash_scripts/have-run/22.11.06_rbar_correct_3.sh
+++ bash_scripts/have-run/22.11.06_rbar_correct_3.sh
@@ -3,11 +3,11 @@ cd ..
 python bash_scripts/progress_bar.py
 runs=(
 # correct rbar pept-func
-# configs/DelayGCN_rbar/peptides-func-DelayGCN_L=15_d=060_rbar=02.yaml
-# configs/DelayGCN_rbar/peptides-func-DelayGCN_L=15_d=060_rbar=03.yaml
-# configs/DelayGCN_rbar/peptides-func-DelayGCN_L=15_d=060_rbar=04.yaml
-configs/DelayGCN_rbar/peptides-func-DelayGCN_L=15_d=060_rbar=05.yaml
-configs/DelayGCN_rbar/peptides-func-DelayGCN_L=15_d=060_rbar=06.yaml
+# configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=02.yaml
+# configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=03.yaml
+# configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=04.yaml
+configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=05.yaml
+configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=06.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
