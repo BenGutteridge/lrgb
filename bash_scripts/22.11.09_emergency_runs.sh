8c4446702ef1e0128b7dc8dd597caa9eb342e128
BenGutteridge <benpgutteridge@gmail.com>
2022-11-11 10:10:05 +0000

    redo emergency runs

--- bash_scripts/22.11.09_emergency_runs.sh
+++ bash_scripts/22.11.09_emergency_runs.sh
@@ -2,10 +2,10 @@
 cd ..
 python bash_scripts/progress_bar.py
 runs=(
-# configs/rbar-GCN/peptides-struct-DelayGCN_L=11_rbar=inf.yaml
-# configs/rbar-GCN/peptides-struct-DelayGCN_L=13_rbar=inf.yaml
-# configs/alphaGCN/peptides-struct-alphaGCN_L=09_d=075.yaml
-# configs/alphaGCN/peptides-struct-alphaGCN_L=11_d=060.yaml
+configs/rbar-GCN/peptides-struct-DelayGCN_L=11_rbar=inf.yaml
+configs/rbar-GCN/peptides-struct-DelayGCN_L=13_rbar=inf.yaml
+configs/alphaGCN/peptides-struct-alphaGCN_L=09_d=075.yaml
+configs/alphaGCN/peptides-struct-alphaGCN_L=11_d=060.yaml
 
 configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=06.yaml
 configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=06.yaml
