d81437a785a593a3946e1f5b0a89dd521e28b631
BenGutteridge <benpgutteridge@gmail.com>
2022-11-12 17:51:42 +0000

    filling in the gaps runs pept func

--- bash_scripts/22.11.12_pept-func_filling_gaps_2.sh
+++ bash_scripts/22.11.12_pept-func_filling_gaps_2.sh
@@ -4,38 +4,14 @@ BATCH="22.11.12_pept-func_filling_gaps"
 mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
-  # r*=6: 15,17,19
-  configs/rbar-GCN/peptides-func-DelayGCN_L=15_d=060_rbar=06.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=06.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=06.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=06.yaml
-  # r*2,3,4: 5,7,9,11,13,17,19
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=06.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=06.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=06.yaml
   # r*=1: 17,19,21
   # r*=inf: 17,19,21
+  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=inf.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=01.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=01.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=inf.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=01.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=inf.yaml
   )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
