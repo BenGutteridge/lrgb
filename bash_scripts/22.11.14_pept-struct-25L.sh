92e43fdd7ea72b19e266dee742eeb5c58a4f5694
BenGutteridge <benpgutteridge@gmail.com>
2022-11-14 12:01:21 +0000

    25 layer struct run w/ r*=24: can it beat Transformer?

---
+++ bash_scripts/22.11.14_pept-struct-25L.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ..
+BATCH="22.11.12_pept-struct_25L"
+mkdir -p "results/$BATCH"
+python bash_scripts/progress_bar.py
+runs=(
+  configs/rbar-GCN/peptides-struct-DelayGCN_L=25_rbar=24.yaml
+  )
+for run in "${runs[@]}" ; do
+  # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "results/$BATCH" optim.max_epoch 300
+  python bash_scripts/progress_bar.py "$run"
+done
