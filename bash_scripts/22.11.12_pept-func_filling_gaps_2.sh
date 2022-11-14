9e1f1dbcaa6e60b5bce2dfd33641e72ab3b52749
BenGutteridge <benpgutteridge@gmail.com>
2022-11-14 09:58:35 +0000

    pept func filling gaps - running two simultaneously on aimscdt1

--- bash_scripts/22.11.12_pept-func_filling_gaps_2.sh
+++ bash_scripts/22.11.12_pept-func_filling_gaps_2.sh
@@ -10,7 +10,7 @@ runs=(
   configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=01.yaml
   configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=01.yaml
   configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=inf.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=01.yaml
+  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=01.yaml # <- RUN ALREADY - STOP IF YOU CAN
   configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=inf.yaml
   )
 for run in "${runs[@]}" ; do
