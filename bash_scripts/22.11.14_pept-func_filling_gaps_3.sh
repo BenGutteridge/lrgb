207d4ca2d2bfda523b9e553d04087a0b924eaa85
BenGutteridge <benpgutteridge@gmail.com>
2022-11-14 22:20:25 +0000

    fixing moved around configs

--- bash_scripts/22.11.14_pept-func_filling_gaps_3.sh
+++ bash_scripts/22.11.14_pept-func_filling_gaps_3.sh
@@ -5,30 +5,30 @@ mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
   # r*2,3,4: 5,7,9,11,13,17,19
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=03.yaml
+  # configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=03.yaml
+  # configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=03.yaml
+  # configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=03.yaml
+  configs/rbar-GCN/rbar=03/peptides-func-DelayGCN_L=11_d=085_rbar=03.yaml
+  configs/rbar-GCN/rbar=03/peptides-func-DelayGCN_L=13_d=070_rbar=03.yaml
+  configs/rbar-GCN/rbar=03/peptides-func-DelayGCN_L=17_rbar=03.yaml
+  configs/rbar-GCN/rbar=03/peptides-func-DelayGCN_L=19_rbar=03.yaml
 
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=02.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=02.yaml
+  configs/rbar-GCN/rbar=02/peptides-func-DelayGCN_L=05_d=175_rbar=02.yaml
+  configs/rbar-GCN/rbar=02/peptides-func-DelayGCN_L=07_d=130_rbar=02.yaml
+  configs/rbar-GCN/rbar=02/peptides-func-DelayGCN_L=09_d=100_rbar=02.yaml
+  configs/rbar-GCN/rbar=02/peptides-func-DelayGCN_L=11_d=085_rbar=02.yaml
+  configs/rbar-GCN/rbar=02/peptides-func-DelayGCN_L=13_d=070_rbar=02.yaml
 
-  configs/rbar-GCN/peptides-func-DelayGCN_L=05_d=175_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=07_d=130_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=09_d=100_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=11_d=085_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=13_d=070_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=17_rbar=04.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=19_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=05_d=175_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=07_d=130_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=09_d=100_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=11_d=085_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=13_d=070_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=17_rbar=04.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=19_rbar=04.yaml
 
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=03.yaml
-  configs/rbar-GCN/peptides-func-DelayGCN_L=21_rbar=04.yaml
+  configs/rbar-GCN/rbar=03/peptides-func-DelayGCN_L=21_rbar=03.yaml
+  configs/rbar-GCN/rbar=04/peptides-func-DelayGCN_L=21_rbar=04.yaml
   # r*=1: 17,19,21
   # r*=inf: 17,19,21
   )
