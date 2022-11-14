6593819eba907d3637553da6956b2077cd3007bc
BenGutteridge <benpgutteridge@gmail.com>
2022-11-14 22:36:14 +0000

    struct more than 21 Ls

---
+++ bash_scripts/22.11.14_pept-struct_L>21.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ..
+BATCH="22.11.14_pept-struct_25L"
+mkdir -p "results/$BATCH"
+python bash_scripts/progress_bar.py
+runs=(
+  configs/rbar-GCN/rbar=01/peptides-struct-DelayGCN_L=23_rbar=01.yaml
+  configs/rbar-GCN/rbar=01/peptides-struct-DelayGCN_L=25_rbar=01.yaml
+  configs/rbar-GCN/rbar=01/peptides-struct-DelayGCN_L=27_rbar=01.yaml
+  configs/rbar-GCN/rbar=01/peptides-struct-DelayGCN_L=29_rbar=01.yaml
+  )
+for run in "${runs[@]}" ; do
+  # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "results/$BATCH" optim.max_epoch 300
+  python bash_scripts/progress_bar.py "$run"
+done
