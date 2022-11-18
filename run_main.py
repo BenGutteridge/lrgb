1483844bb9b10595bfc0b253b80cb3c2851d8a8d
BenGutteridge <benpgutteridge@gmail.com>
2022-11-18 13:03:16 +0000

    first working QM9 run

--- run_main.py
+++ run_main.py
@@ -119,7 +119,7 @@ def run_loop_settings():
 # # PCQM-Contact
 # argpath = "/Users/beng/Documents/lrgb/configs/%s/pcqm-contact-%s.yaml" % (model, model)
 
-argpath = 'configs/rbar-GCN/rbar>20/peptides-struct-DelayGCN_L=21_rbar=20.yaml'
+argpath = 'configs/GCN/QM9-GCN_L=02_d=500.yaml'
 
 repeat = 1
 import argparse
