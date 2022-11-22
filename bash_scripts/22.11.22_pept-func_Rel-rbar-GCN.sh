5b02ee0f5be45556c4670f564fa9daf94dfc7417
BenGutteridge <benpgutteridge@gmail.com>
2022-11-22 18:22:26 +0000

    fixing error with wrong stage type

--- bash_scripts/22.11.22_pept-func_Rel-rbar-GCN.sh
+++ bash_scripts/22.11.22_pept-func_Rel-rbar-GCN.sh
@@ -15,6 +15,6 @@ configs/rbar-GCN/rbar=inf/peptides-func-DelayGCN_L=11_d=085_rbar=inf.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "results/$BATCH" optim.max_epoch 300 use_edge_labels True gnn.stage_type rel_delay_gnn
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir dataset.dir /data/beng/datasets "results/$BATCH" optim.max_epoch 300 use_edge_labels True gnn.stage_type rel_delay_gnn
   python bash_scripts/progress_bar.py "$run"
 done
