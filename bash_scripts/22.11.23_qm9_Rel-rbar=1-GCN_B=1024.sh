9dd0afdb92a9c9c86390a971b0edb37dda04b626
BenGutteridge <benpgutteridge@gmail.com>
2022-11-23 11:45:12 +0000

    qm9 with larger batch size

---
+++ bash_scripts/22.11.23_qm9_Rel-rbar=1-GCN_B=1024.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ..
+BATCH="qm9_2_r=1_edges"
+mkdir -p "results/$BATCH"
+python bash_scripts/progress_bar.py
+runs=(
+  # alphaGCN 5 layers multi runs to see if its consistent
+  # stretched QM9 for everything
+  configs/rbar-GCN/rbar=01/QM9-rGCN_L=05_r=01.yaml
+  configs/rbar-GCN/rbar=01/QM9-rGCN_L=07_r=01.yaml
+  configs/rbar-GCN/rbar=01/QM9-rGCN_L=09_r=01.yaml
+  configs/rbar-GCN/rbar=01/QM9-rGCN_L=11_r=01.yaml
+)
+for run in "${runs[@]}" ; do
+  # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "results/$BATCH" optim.max_epoch 300 use_edge_labels True gnn.stage_type rel_delay_gnn train.batch_size 1024
+  python bash_scripts/progress_bar.py "$run"
+done
