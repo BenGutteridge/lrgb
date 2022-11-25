c8a2c2c154bbe0500ba6726b9bcf3eedbbf2977b
BenGutteridge <benpgutteridge@gmail.com>
2022-11-25 17:18:03 +0000

    comparing rel and reg

--- bash_scripts/22.11.25_broken_Rel_tests.sh
+++ bash_scripts/22.11.25_broken_Rel_tests.sh
@@ -9,6 +9,6 @@ runs=(
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$BATCH"
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "results/$BATCH"
   python bash_scripts/progress_bar.py "$run"
 done
