48bd5da7f41b429a24b46df73d433da673d5626e
BenGutteridge <benpgutteridge@gmail.com>
2022-11-25 17:16:46 +0000

    comparing rel and reg

--- bash_scripts/22.11.25_broken_Rel_tests.sh
+++ bash_scripts/22.11.25_broken_Rel_tests.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 cd ..
-BATCH="qm9_stragglers_r=inf_B=1024"
+BATCH="debugging_rel"
 mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
-  # regular peptides func, 5 layers, hidden_dim=64, rbar=inf
-  # normal DelayGCN using delay_gnn stage
-  # using rel_delay_gnn but edited to (hopefully) be identical
-
+  configs/debug_cfgs/peptides-func-DelayGCN_L=05_d=064_rbar=inf.yaml
+  # configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$BATCH" optim.max_epoch 300 train.batch_size 1024
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$BATCH"
   python bash_scripts/progress_bar.py "$run"
 done
