38ad331f408b8dad425774e21d29b33da8a9726f
BenGutteridge <benpgutteridge@gmail.com>
2022-11-25 17:18:35 +0000

    comparing rel and reg

--- bash_scripts/22.11.25_broken_Rel_tests.sh
+++ bash_scripts/22.11.25_broken_Rel_tests.sh
@@ -4,8 +4,8 @@ BATCH="debugging_rel"
 mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
-  configs/debug_cfgs/peptides-func-DelayGCN_L=05_d=064_rbar=inf.yaml
-  # configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf.yaml
+  # configs/debug_cfgs/peptides-func-DelayGCN_L=05_d=064_rbar=inf.yaml
+  configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
