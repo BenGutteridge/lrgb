94671af3f2ad264f6accc2b3337d3a6c8406a539
BenGutteridge <benpgutteridge@gmail.com>
2022-11-25 23:42:53 +0000

    v8

--- bash_scripts/22.11.25_broken_Rel_tests.sh
+++ bash_scripts/22.11.25_broken_Rel_tests.sh
@@ -18,10 +18,10 @@ runs=(
   # configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf_Rel_V6.yaml
 
   # using W_edge but with A(1) not A_e -- one step away from original Rel-r*GCN
-  configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf_Rel_V7.yaml
+  # configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf_Rel_V7.yaml
 
   # using W_kt with e parts, a la V6, but using A_e now -- this should be a finished version, if it works (still no idea what's wrong w V7 but oh well?)
-    configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf_Rel_V8.yaml
+  configs/debug_cfgs/peptides-func-RelDelayGCN_L=05_d=064_rbar=inf_Rel_V8.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
