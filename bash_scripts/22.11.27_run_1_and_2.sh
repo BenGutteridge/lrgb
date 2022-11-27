37934495a2cc4b727f68c9c714c8da6a18a23daf
BenGutteridge <benpgutteridge@gmail.com>
2022-11-27 23:42:03 +0000

    some QM9 run cfgs (not Rel)

--- bash_scripts/22.11.27_run_1_and_2.sh
+++ bash_scripts/22.11.27_run_1_and_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash bash_scripts/22.11.27_QM9_rGCN_stragglers_1_r*=1.sh
-bash bash_scripts/22.11.27_QM9_rGCN_stragglers_2_r*=inf.sh
-# bash_scripts/22.11.27_QM9_rGCN_stragglers_3_alpha.sh
-# bash_scripts/22.11.27_QM9_rGCN_stragglers_4_r*=3.sh
+bash 22.11.27_QM9_rGCN_stragglers_1_r*=1.sh
+bash 22.11.27_QM9_rGCN_stragglers_2_r*=inf.sh
+# bash 22.11.27_QM9_rGCN_stragglers_3_alpha.sh
+# bash 22.11.27_QM9_rGCN_stragglers_4_r*=3.sh
