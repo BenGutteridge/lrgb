50246cfc089104b095a831e5e58dea64995e3821
BenGutteridge <benpgutteridge@gmail.com>
2022-11-27 14:49:37 +0000

    rerunning aimscdt2's r*=1 runs for rel

--- bash_scripts/22.11.26_rel-rGCN-Lite_runs.sh
+++ bash_scripts/22.11.26_rel-rGCN-Lite_runs.sh
@@ -4,9 +4,9 @@ BATCH="pept-func-rel_rGCN_lite_r*=1_d=128"
 mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=05_d=175_rbar=01.yaml
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=07_d=130_rbar=01.yaml
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=09_d=100_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=05_d=175_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=07_d=130_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=09_d=100_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=11_d=085_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=13_d=070_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=15_d=060_rbar=01.yaml
@@ -20,6 +20,6 @@ configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=29_rbar=01.yaml
 )
 for run in "${runs[@]}" ; do
   # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$BATCH" optim.max_epoch 300 use_edge_labels True gnn.stage_type rel_delay_gnn_lite gnn.dim_inner 128 train.batch_size 256
+  python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$BATCH" optim.max_epoch 300 use_edge_labels True gnn.stage_type rel_delay_gnn_lite gnn.dim_inner 128
   python bash_scripts/progress_bar.py "$run"
 done
