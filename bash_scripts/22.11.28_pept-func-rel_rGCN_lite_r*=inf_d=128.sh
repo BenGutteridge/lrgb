ffaca1664472817645d5941befb7c04683f17c99
BenGutteridge <benpgutteridge@gmail.com>
2022-11-28 11:01:23 +0000

    rerunning broken config

--- bash_scripts/22.11.28_pept-func-rel_rGCN_lite_r*=inf_d=128.sh
+++ bash_scripts/22.11.28_pept-func-rel_rGCN_lite_r*=inf_d=128.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ..
-BATCH="pept-func-rel_rGCN_lite_r*=inf_d=128"
+BATCH="cdt1_pept-func-rel_rGCN_lite_r*=inf_d=128"
 mkdir -p "results/$BATCH"
 python bash_scripts/progress_bar.py
 runs=(
   # don't be fooled by filenames, they've all had rbar set to inf manually
 # configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=05_d=175_rbar=01.yaml
 # configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=07_d=130_rbar=01.yaml
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=09_d=100_rbar=01.yaml
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=11_d=085_rbar=01.yaml
-configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=13_d=070_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=09_d=100_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=11_d=085_rbar=01.yaml
+# configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=13_d=070_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=15_d=060_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=17_rbar=01.yaml
 configs/rbar-GCN/rbar=01/peptides-func-DelayGCN_L=19_rbar=01.yaml
