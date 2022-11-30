9c983ac5b0c1f08624fd60ffbdb9bb755bd90e66
BenGutteridge <benpgutteridge@gmail.com>
2022-11-30 14:59:13 +0000

    alphaGCN for different BS

--- bash_scripts/22.11.30_compare_QM9_alphaGCN_batch_size_d=064.sh
+++ bash_scripts/22.11.30_compare_QM9_alphaGCN_batch_size_d=064.sh
@@ -16,8 +16,8 @@ batch_sizes=(
 )
 
 for bs in "${batch_sizes[@]}" ; do
-  mkdir -p "results/$DIR_NAME_bs=$bs"
-  DIR="results/$DIR_NAME_bs=$bs"
+  mkdir -p "results/$DIR_NAME\_=$bs"
+  DIR="results/$DIR_NAME\_bs=$bs"
   for run in "${runs[@]}" ; do
     # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
     python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "results/$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner 64
