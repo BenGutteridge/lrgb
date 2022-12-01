9e0b4b635fcc6907ebec9a018725c7848fcf2e4d
BenGutteridge <benpgutteridge@gmail.com>
2022-12-01 12:51:27 +0000

    d vs BS experiments

--- bash_scripts/22.11.30_QM9_d_vs_BS.sh
+++ bash_scripts/22.11.30_QM9_d_vs_BS.sh
@@ -25,14 +25,16 @@ num_layers=(
   14
 )
 
-for d in "${hidden_dims[@]}" ; do
-  for bs in "${batch_sizes[@]}" ; do
-    DIR="results/$DIR_NAME\_bs=$bs"
-    mkdir -p DIR
-    for run in "${runs[@]}" ; do
-      # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-      python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner 64
-      python bash_scripts/progress_bar.py "$run"
+for L in "${num_layers[@]}" ; do
+  for d in "${hidden_dims[@]}" ; do
+    for bs in "${batch_sizes[@]}" ; do
+      DIR="results/$DIR_NAME\_bs=$bs"
+      mkdir -p DIR
+      for run in "${runs[@]}" ; do
+        # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
+        python main.py --cfg "$run" --repeat 1 device cuda dataset.dir /data/beng/datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$L"
+        python bash_scripts/progress_bar.py "$run"
+      done
     done
   done
 done
