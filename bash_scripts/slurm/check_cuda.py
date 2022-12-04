093d29e6971f1dc8603b0d5a8e079f2516d865e1
BenGutteridge <benpgutteridge@gmail.com>
2022-12-04 12:31:01 +0000

    slurm script test for jade2

--- bash_scripts/slurm/check_cuda.py
+++ bash_scripts/slurm/check_cuda.py
@@ -1,15 +1,12 @@
-try:
-  import torch
-  print("""
-  torch.cuda.is_available()
-  torch.cuda.current_device()
-  torch.cuda.device(0)
-  torch.cuda.device_count()
-  torch.cuda.get_device_name(0)""", 
-  torch.cuda.is_available(),
-  torch.cuda.current_device(),
-  torch.cuda.device(0),
-  torch.cuda.device_count(),
-  torch.cuda.get_device_name(0))
-except:
-  print('Failed')
+import torch
+print("""
+torch.cuda.is_available()
+torch.cuda.current_device()
+torch.cuda.device(0)
+torch.cuda.device_count()
+torch.cuda.get_device_name(0)""")
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
