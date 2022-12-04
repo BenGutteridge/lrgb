404f4a6c1c149752733888636ff3c0bfb9144c78
BenGutteridge <benpgutteridge@gmail.com>
2022-12-04 21:37:41 +0000

    aims vs jade speed test

--- bash_scripts/slurm/22.12.04_jade_speed_test.sh
+++ bash_scripts/slurm/22.12.04_jade_speed_test.sh
@@ -3,7 +3,7 @@ cd ../..
 DIR_NAME="jade_speed_test"
 python bash_scripts/progress_bar.py
 runs=(
-configs/rbar-GCN/rbar=inf/peptides-func-DelayGCN_L=05_d=175_rbar=inf.yaml
+configs/rbar-GCN/rbar=01/QM9-rGCN_L=05_r=inf.yaml
 )
 batch_sizes=(
   128
@@ -18,9 +18,10 @@ num_layers=(
 for L in "${num_layers[@]}" ; do
   for d in "${hidden_dims[@]}" ; do
     for bs in "${batch_sizes[@]}" ; do
-      DIR="results/$DIR_NAME\_bs=$bs"
+      DIR="results/$DIR_NAME\_bs=$bs\_d=$d\_L=$L"
       mkdir -p DIR
       for run in "${runs[@]}" ; do
+        echo "RUN: $DIR"
         # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
         python3 main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$L"
         python3 bash_scripts/progress_bar.py "$run"
