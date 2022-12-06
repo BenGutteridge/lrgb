d678c53af7ad1862d8c442700756679930a4ba28
BenGutteridge <benpgutteridge@gmail.com>
2022-12-06 11:06:01 +0000

    running qm9 in parallel on jade

--- bash_scripts/slurm/22.12.06_jade_QM9_rbar=inf.sh
+++ bash_scripts/slurm/22.12.06_jade_QM9_rbar=inf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../..
-DIR_NAME="22.12.05_jade_QM9_alpha_REALLY_QM9"
+DIR_NAME="22.12.06_jade_QM9_rbar=half_L"
 runs=(
-configs/alphaGCN/QM9-alphaGCN_L=05.yaml
+configs/rbar-GCN/QM9-rGCN.yaml
 )
 batch_sizes=(
   128
@@ -14,6 +14,8 @@ hidden_dims=(
 
 L="$1"
 
+rbar="-1"
+echo "rbar: $rbar"
 
 for d in "${hidden_dims[@]}" ; do
   for bs in "${batch_sizes[@]}" ; do
@@ -22,7 +24,7 @@ for d in "${hidden_dims[@]}" ; do
     for run in "${runs[@]}" ; do
       echo "RUN: $DIR"
       # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-      python3 main.py --cfg "$run" --repeat 3 device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$1"
+      python3 main.py --cfg "$run" --repeat 3 rbar "$rbar" device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$1"
       mv "$DIR" "results/complete/"
     done
   done
--- bash_scripts/slurm/run_array.sh
+++ bash_scripts/slurm/run_array.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=36:00:00
 
 # set name of job
-#SBATCH --job-name=jade_qm9_alpha
+#SBATCH --job-name=jade_qm9_rbars
 
 # set number of GPUs
 #SBATCH --gres=gpu:1
@@ -26,4 +26,9 @@ conda activate lrgb2
 conda info --
 echo $CONDA_DEFAULT_ENV
 # conda list
-bash 22.12.05_jade_QM9_alpha.sh "${SLURM_ARRAY_TASK_ID}"
+# bash 22.12.05_jade_QM9_alpha.sh "${SLURM_ARRAY_TASK_ID}"
+bash 22.12.06_jade_QM9_rbar=01.sh "${SLURM_ARRAY_TASK_ID}" &
+      bash 22.12.06_jade_QM9_rbar=03.sh "${SLURM_ARRAY_TASK_ID}" &
+      bash 22.12.06_jade_QM9_rbar=half_L.sh "${SLURM_ARRAY_TASK_ID}" &
+      bash 22.12.06_jade_QM9_rbar=inf.sh "${SLURM_ARRAY_TASK_ID}" &
+      bash 22.12.06_jade_QM9_rbar=L-2.sh "${SLURM_ARRAY_TASK_ID}"
