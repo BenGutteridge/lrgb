04099041974f710c8c92331456cd5e84e2f26c6f
BenGutteridge <benpgutteridge@gmail.com>
2022-12-06 23:14:55 +0000

    Rel parallel jade

---
+++ bash_scripts/slurm/22.12.06_jade_QM9_Rel-rbar=01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cd ../..
+DIR_NAME="22.12.06_jade_QM9_rbar=01"
+runs=(
+configs/rbar-GCN/QM9-rGCN.yaml
+)
+batch_sizes=(
+  128
+  # 256
+)
+hidden_dims=(
+  64
+)
+
+L="$1"
+
+rbar=1
+echo "rbar: $rbar"
+
+for d in "${hidden_dims[@]}" ; do
+  for bs in "${batch_sizes[@]}" ; do
+    DIR="results/$DIR_NAME\_bs=$bs\_d=$d\_L=$L"
+    mkdir -p DIR
+    for run in "${runs[@]}" ; do
+      echo "RUN: $DIR"
+      # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
+      python3 main.py --cfg "$run" --repeat 3 rbar "$rbar" gnn.stage_type rel_delay_gnn device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$1"
+      mv "$DIR" "results/complete/"
+    done
+  done
+done
