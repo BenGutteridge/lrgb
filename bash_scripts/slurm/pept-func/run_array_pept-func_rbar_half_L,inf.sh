d28188796cf6603716af56a08c31bb737ecbedb5
BenGutteridge <benpgutteridge@gmail.com>
2022-12-08 11:59:21 +0000

    trying pept func on jade again

---
+++ bash_scripts/slurm/pept-func/run_array_pept-func_rbar_half_L,inf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=72:00:00
+
+# set name of job
+#SBATCH --job-name=pf-rinf,L\2
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=gutterboyben@gmail.com
+
+# run the application
+module load cuda/10.2
+module load python/anaconda3
+source $condaDotFile
+conda activate lrgb2
+conda info --
+echo $CONDA_DEFAULT_ENV
+# conda list
+# bash 22.12.05_jade_pept-func_alpha.sh "${SLURM_ARRAY_TASK_ID}"
+L="${SLURM_ARRAY_TASK_ID}"
+# bash 22.12.06_jade_pept-func_rbar=01.sh "$L"
+# bash 22.12.06_jade_pept-func_rbar=03.sh "$L" &
+bash 22.12.06_jade_pept-func_rbar=half_L.sh "$L" & 
+      bash 22.12.06_jade_pept-func_rbar=inf.sh "$L"
+# bash 22.12.06_jade_pept-func_rbar=L-2.sh "$L"
+# bash 22.12.06_jade_pept-func_alpha.sh "$L"
