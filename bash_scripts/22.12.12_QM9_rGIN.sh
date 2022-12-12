8499ea1506c0573c8ef4751c49c25e32838904f1
BenGutteridge <benpgutteridge@gmail.com>
2022-12-12 12:09:27 +0000

    running r*GIN on verges, first go

--- bash_scripts/22.12.12_QM9_rGIN.sh
+++ bash_scripts/22.12.12_QM9_rGIN.sh
@@ -20,7 +20,7 @@ for L in "${num_layers[@]}" ; do
       mkdir -p DIR
       for run in "${runs[@]}" ; do
         # python main.py --cfg "$run" --repeat 3 device cuda dataset.dir /data/beng/datasets train.batch_size 16
-        python main.py --cfg "$run" --repeat 3 device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$L"
+        python main.py --cfg "$run" --repeat 1 device cuda dataset.dir datasets out_dir "$DIR" optim.max_epoch 300 train.batch_size "$bs" gnn.dim_inner "$d" gnn.layers_mp "$L"
         # python bash_scripts/progress_bar.py "$run"
       done
     done
