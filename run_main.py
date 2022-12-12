ea06a91342efc8f68f81b779fc6804e6ae2a7717
BenGutteridge <benpgutteridge@gmail.com>
2022-12-12 14:56:49 +0000

    GINE QM9 for comparison (bs-1024)

--- run_main.py
+++ run_main.py
@@ -126,8 +126,8 @@ def run_loop_settings():
 # argpath = 'configs/alphaGCN/QM9-alphaGCN_L=13.yaml'
 # argpath = 'configs/DelayGCN/500k_stretched/peptides-func-DelayGCN_L=07_d=130.yaml'
 # argpath = 'configs/rbar-GCN/QM9-rGCN.yaml'
-# argpath = 'configs/GINE/peptides-func-GINE.yaml'
-argpath = 'configs/rbar-GIN/QM9-r*GIN.yaml'
+argpath = 'configs/GINE/peptides-func-GINE.yaml'
+# argpath = 'configs/rbar-GIN/QM9-r*GIN.yaml'
 
 repeat = 1
 import argparse
@@ -145,10 +145,11 @@ def parse_args() -> argparse.Namespace:
                         help='See graphgym/config.py for remaining options.')
 
     extra_args = [
-        'optim.max_epoch 5',
+        'optim.max_epoch 1',
         # 'gnn.layer_type delay_gineconv',
         # 'model.type flattened_delay_gine',
-        'model.type flattened_delay_gin',
+        # 'model.type flattened_delay_gin',
+        'model.type flattened_gine',
         # 'gnn.stage_type rel_delay_gnn',
         # 'rbar 3',
         'gnn.dim_inner 16',
@@ -157,7 +158,7 @@ def parse_args() -> argparse.Namespace:
         'dataset.dir datasets',
         # 'device cuda',
         # 'alpha 6',
-        'dataset.edge_encoder False'
+        # 'dataset.edge_encoder False'
         # 'use_edge_labels True',
         ]
     extra_args = ' '.join(extra_args)
