2aa2d11d92486359c28edc2411fcf0999b7325d6
BenGutteridge <benpgutteridge@gmail.com>
2022-12-14 00:08:09 +0000

    submitting jobs to slurm

--- bash_scripts/crucial_1.sh
+++ bash_scripts/crucial_1.sh
@@ -2,6 +2,6 @@
 
 bash 22.12.13_QM9_R-SPN.sh 8 10 & 
   bash 22.12.13_QM9_R-SPN.sh 8 5 & 
-  bash 22.12.13_QM9_R\*-SPN.sh 8 1 & 
+  bash 22.12.13_QM9_R\*-SPN.sh 8 1
   # bash 22.12.13_QM9_R\*-SPN.sh 8 "-1" &
   # bash 22.12.13_QM9_R\*-SPN.sh 8 4
