96b38e97e3f55d017dd442f18d174519b6d27cee
BenGutteridge <benpgutteridge@gmail.com>
2023-01-02 15:34:56 +0000

    posenc jade runs attempt

---
+++ bash_scripts/slurm_run_rwse.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=36:00:00
+
+# set name of job
+#SBATCH --job-name=posenc
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=gutterboyben@gmail.com
+
+# run the application
+module load cuda/10.2
+module load python/anaconda3
+source $condaDotFile
+conda activate lrgb2
+conda info --
+echo $CONDA_DEFAULT_ENV
+# conda list
+# bash 22.12.05_jade_QM9_alpha.sh "${SLURM_ARRAY_TASK_ID}"
+bash run_rwse.sh
