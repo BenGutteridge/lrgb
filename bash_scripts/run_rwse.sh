77796577601c36a658b17bfa3a5b946f50f27241
BenGutteridge <benpgutteridge@gmail.com>
2023-01-02 15:45:01 +0000

    posenc jade runs attempt

--- bash_scripts/run_rwse.sh
+++ bash_scripts/run_rwse.sh
@@ -12,5 +12,5 @@ L=$2
 
 for file in "${files[@]}"
 do
-  python3.9 main.py --cfg "$file" device cuda dataset.dir datasets rbar "$rbar" gnn.layers_mp "$L"
+  python3.8 main.py --cfg "$file" device cuda dataset.dir datasets rbar "$rbar" gnn.layers_mp "$L"
 done
