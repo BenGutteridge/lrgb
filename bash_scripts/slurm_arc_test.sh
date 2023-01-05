6172310acb51bedde992b4fc563b0a3789c51865
BenGutteridge <benpgutteridge@gmail.com>
2023-01-05 16:00:44 +0000

    first slurm arc run

--- bash_scripts/slurm_arc_test.sh
+++ bash_scripts/slurm_arc_test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#SBATCH --job-name=lrgb
+#SBATCH --job-name=smallrgb
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=12:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=short
 # must be on htc, only one w/ GPUs
 #SBATCH --clusters=htc
@@ -13,4 +13,5 @@ module load Anaconda3
 module load CUDA/11.3
 source activate $DATA/lrgb
 python -c "import torch; print(torch.__version__)"
-bash run_ps_exp.sh LapPE -1 9
+# bash run_ps_exp.sh LapPE -1 9
+python main.py --cfg configs/rbar-GCN/peptides-struct-DelayGCN+LapPE.yaml device cuda dataset.dir datasets rbar -1 gnn.layers_mp 3 optim.max_epoch 5 gnn.dim_inner 16
