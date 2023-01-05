28469de461ed82f554022e017e897a79e06a256d
BenGutteridge <benpgutteridge@gmail.com>
2023-01-05 14:11:07 +0000

    first slurm arc run

--- bash_scripts/slurm_arc_test.sh
+++ bash_scripts/slurm_arc_test.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --job-name=test_lrgb
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=00:10:00
 #SBATCH --partition=devel
-
-module load mpitest
-
-mpirun mpihello
+# must be on htc, only one w/ GPUs
+#SBATCH --clusters=htc
+# set number of GPUs
+#SBATCH --gres=gpu:1
+cd $DATA/repos/lrgb
+module load Anaconda3
+module load CUDA/11.3
+source activate $DATA/lrgb
+python -c "import torch; print(torch.__version__)"
+bash run_ps_exp.sh LapPE -1 9
