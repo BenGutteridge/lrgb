a44a19c00814b6ac9a727eeb253b19c08aa0f32c
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 20:04:10 +0000

    runing lrgb on short

---
+++ bash_scripts/slurm_arc_short.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#SBATCH --job-name=more_r*_struct
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=12:00:00
+#SBATCH --partition=short
+# must be on htc, only one w/ GPUs
+#SBATCH --clusters=htc
+# set number of GPUs
+#SBATCH --gres=gpu:1
+#SBATCH --account=engs-oxnsg
+cd $DATA/repos/lrgb/bash_scripts
+pe=$1
+rbar=$2
+module load Anaconda3
+module load CUDA/11.2
+source activate $DATA/lrgb
+nvcc --version
+python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+
+layers=(7 11 15 19)
+dims=(130 85 64 50)
+bash run_struct_pe_exp.sh $pe $rbar layers[$SLURM_ARRAY_TASK_ID] dims[$SLURM_ARRAY_TASK_ID] datasets
