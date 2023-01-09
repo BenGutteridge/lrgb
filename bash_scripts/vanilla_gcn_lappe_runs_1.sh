fab6351eb8d8302a19955cf30d99f440c471e3be
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 19:33:08 +0000

    some nondelay runs for aimscdt1 and 2

--- bash_scripts/vanilla_gcn_lappe_runs_1.sh
+++ bash_scripts/vanilla_gcn_lappe_runs_1.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 cd ..
 # python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 5 gnn.dim_inner 300
-python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 9 gnn.dim_inner 230
-python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 13 gnn.dim_inner 190
+python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 9 gnn.dim_inner 230 optim.max_epoch 300
+python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 13 gnn.dim_inner 190 optim.max_epoch 300
 # python main.py --cfg configs/GCN/peptides-struct-GCN+LapPE.yaml --repeat 3 gnn.layers_mp 17 gnn.dim_inner 170
