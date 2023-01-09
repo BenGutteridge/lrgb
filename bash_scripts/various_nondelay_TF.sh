2031b046cd5199bda247e12d85a52413976b6e90
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 19:31:03 +0000

    some nondelay runs for aimscdt1 and 2

--- bash_scripts/various_nondelay_TF.sh
+++ bash_scripts/various_nondelay_TF.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 python main.py --cfg configs/Transformer/peptides-struct-Transformer+LapPE.yaml --repeat 3 optim.max_epoch 300
 python main.py --cfg configs/SAN/peptides-struct-SAN+LapPE.yaml --repeat 3 optim.max_epoch 300
 
--- bash_scripts/various_nondelay_gated.sh
+++ bash_scripts/various_nondelay_gated.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 python main.py --cfg configs/GatedGCN/peptides-struct-GatedGCN+RWSE.yaml --repeat 3 optim.max_epoch 300
 python main.py --cfg configs/GatedGCN/peptides-struct-GatedGCN+LapPE.yaml --repeat 3 optim.max_epoch 300
 
