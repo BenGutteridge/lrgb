00a6a800d0651e05c36bf2ef5ed8e46fcd26e784
BenGutteridge <benpgutteridge@gmail.com>
2023-01-10 15:31:43 +0000

    more inbetween runs, r*=1,L/2

--- bash_scripts/cdt1_lappe.sh
+++ bash_scripts/cdt1_lappe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # r*=1 L=5,9,13,17 for fixed 500k params
-bash run_struct_pe_exp.sh LapPE 1 5 175 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 1 9 105 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 1 13 72 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 1 17 55 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 1 7 130 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 1 11 85 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 1 15 64 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 1 19 50 data/beng/datasets
--- bash_scripts/cdt2_lappe.sh
+++ bash_scripts/cdt2_lappe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # r*=L/2 (rounded down), L=5,9,13,17 for fixed 500k params
-bash run_struct_pe_exp.sh LapPE 2 5 175 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 4 9 105 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 6 13 72 data/beng/datasets
-bash run_struct_pe_exp.sh LapPE 8 17 55 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 3 7 130 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 5 11 85 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 7 15 64 data/beng/datasets
+bash run_struct_pe_exp.sh LapPE 9 19 50 data/beng/datasets
