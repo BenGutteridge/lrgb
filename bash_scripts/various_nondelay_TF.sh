8ec9c4a3406c2711192d8ce5b0716e6ee2cb7c88
BenGutteridge <benpgutteridge@gmail.com>
2023-01-10 15:17:36 +0000

    bs=64, can't run it on aims otherwise

--- bash_scripts/various_nondelay_TF.sh
+++ bash_scripts/various_nondelay_TF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-python main.py --cfg configs/Transformer/peptides-struct-Transformer+LapPE.yaml --repeat 3 optim.max_epoch 300 device cuda dataset.dir /data/beng/datasets
+python main.py --cfg configs/Transformer/peptides-struct-Transformer+LapPE.yaml --repeat 3 optim.max_epoch 300 device cuda dataset.dir /data/beng/datasets train.batch_size 64
 python main.py --cfg configs/SAN/peptides-struct-SAN+LapPE.yaml --repeat 3 optim.max_epoch 300 device cuda dataset.dir /data/beng/datasets
 
 
