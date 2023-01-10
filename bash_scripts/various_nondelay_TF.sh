11e561b13e0cba1dda65f2689ef7ef0a1ef6a6bd
BenGutteridge <benpgutteridge@gmail.com>
2023-01-10 15:18:27 +0000

    switching tf run to verges

--- bash_scripts/various_nondelay_TF.sh
+++ bash_scripts/various_nondelay_TF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
-python main.py --cfg configs/Transformer/peptides-struct-Transformer+LapPE.yaml --repeat 3 optim.max_epoch 300 device cuda dataset.dir /data/beng/datasets train.batch_size 64
-python main.py --cfg configs/SAN/peptides-struct-SAN+LapPE.yaml --repeat 3 optim.max_epoch 300 device cuda dataset.dir /data/beng/datasets
+python main.py --cfg configs/Transformer/peptides-struct-Transformer+LapPE.yaml --repeat 3 optim.max_epoch 500 device cuda dataset.dir datasets
+python main.py --cfg configs/SAN/peptides-struct-SAN+LapPE.yaml --repeat 3 optim.max_epoch 500 device cuda dataset.dir datasets
 
 
 
