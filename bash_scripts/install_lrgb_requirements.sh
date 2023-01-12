6d3516810bd2b4e4057f345299b6950ab29c6dc8
BenGutteridge <benpgutteridge@gmail.com>
2023-01-12 21:12:07 +0000

    install lrgb requirements

--- bash_scripts/install_lrgb_requirements.sh
+++ bash_scripts/install_lrgb_requirements.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-conda install pytorch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 cudatoolkit=11.3 -c pytorch -c conda-forge
-conda install pyg=2.0.2 -c pyg -c conda-forge
-conda install pandas scikit-learn
-conda install openbabel fsspec rdkit -c conda-forge
-conda install -c dglteam dgl-cuda11.3
+conda -y install pytorch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+conda -y install pyg=2.0.2 -c pyg -c conda-forge
+conda -y install pandas scikit-learn
+conda -y install openbabel fsspec rdkit -c conda-forge
+conda -y install -c dglteam dgl-cuda11.3
 pip install performer-pytorch
 pip install torchmetrics==0.7.2
 pip install ogb
