c4b3161dad9912f364fab61ceab250d6c57eed78
BenGutteridge <benpgutteridge@gmail.com>
2023-01-14 18:11:43 +0000

    runs

--- bash_scripts/pept_struct_runs.sh
+++ bash_scripts/pept_struct_runs.sh
@@ -6,13 +6,13 @@ echo "Ready to run!"
 cd ..
 pe=LapPE
 file="configs/rbar-GCN/peptides-struct-DelayGCN+${pe}.yaml"
-# dir="datasets"
-dir="/data/beng/datasets"
+dir="datasets"
+# dir="/data/beng/datasets"
 
 SLURM_ARRAY_TASK_ID=$2
 
 layers=(5 6 7 8 9 10 11 13 15 17 19 21 23)
 dims=(175 150 130 114 105 92 85 72 64 55 50 42)
-rbars=(-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+rbars=(2 3 4 5 6 7 8 9 10 11)
 
 python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar ${rbars[$SLURM_ARRAY_TASK_ID]} gnn.layers_mp ${layers[$SLURM_ARRAY_TASK_ID]} optim.max_epoch 300 gnn.dim_inner ${dims[$SLURM_ARRAY_TASK_ID]} tensorboard_each_run False train.mode my_custom
