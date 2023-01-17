7ade7a8e184711872946aad5cc6e12533d7dea88
BenGutteridge <benpgutteridge@gmail.com>
2023-01-17 08:48:59 +0000

    runs

--- bash_scripts/fixed_d_runs_r*.sh
+++ bash_scripts/fixed_d_runs_r*.sh
@@ -6,11 +6,6 @@ dir='datasets'
 # fixed d r*=1
 file="configs/rbar-GCN/peptides-struct-DelayGCN+LapPE.yaml"
 
-python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 5 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 7 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 9 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar -1 gnn.layers_mp 5 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar -1 gnn.layers_mp 7 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar -1 gnn.layers_mp 9 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar 3 gnn.layers_mp 7 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-
+python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
+python main.py --cfg "$file" --repeat 3 rbar -1 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
+python main.py --cfg "$file" --repeat 3 rbar 5 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
