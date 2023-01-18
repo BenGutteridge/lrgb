e306c922a303e7e8b0e98ef53df1733c45b2f61e
BenGutteridge <benpgutteridge@gmail.com>
2023-01-18 16:26:37 +0000

    runs

--- bash_scripts/fixed_d_runs.sh
+++ bash_scripts/fixed_d_runs.sh
@@ -11,6 +11,6 @@ file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 dir="/data/beng/datasets"
 d=64
 L=$1
-rbar=$2
+rbar=1
 # echo "r*=$rbar"
 python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
