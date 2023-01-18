e13dbbde2f1cb6d8e9f393e721bbea44827326b5
BenGutteridge <benpgutteridge@gmail.com>
2023-01-18 14:20:39 +0000

    runs

--- bash_scripts/fixed_d_runs_r*.sh
+++ bash_scripts/fixed_d_runs_r*.sh
@@ -4,8 +4,7 @@ cd ..
 dir="/data/beng/datasets"
 # dir='datasets'
 # fixed d r*=1
-file="configs/rbar-GCN/peptides-struct-DelayGCN+LapPE.yaml"
+pe=none
+file="configs/rbar-GCN/peptides-func-DelayGCN+$pe.yaml"
 
-python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar -1 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
-python main.py --cfg "$file" --repeat 3 rbar 5 gnn.layers_mp 11 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
+python main.py --cfg "$file" --repeat 3 rbar 1 gnn.layers_mp 23 optim.max_epoch 300 gnn.dim_inner 96 tensorboard_each_run False train.mode my_custom device cuda dataset.dir $dir
