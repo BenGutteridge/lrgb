85dfa2ee389e54783b53c08e256393de8547c820
BenGutteridge <benpgutteridge@gmail.com>
2023-01-19 13:14:43 +0000

    runs

--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=Sd=64rL/2
+#SBATCH --job-name=F.d=64rL/2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -18,6 +18,7 @@ python -c "import torch; print(torch.__version__); print(torch.cuda.is_available
 pe=none
 # task='func'
 task='struct'
+rbar=1
 # file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
 file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
@@ -28,6 +29,4 @@ L=$SLURM_ARRAY_TASK_ID
 # rbar=-1
 rbar=$(($SLURM_ARRAY_TASK_ID/2))
 echo "r*=$rbar"
-python3.9 main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
-
-# python3.9 main.py --cfg file="configs/rbar-GCN/peptides-struct-DelayGCN+none.yaml" --repeat 3 device cuda dataset.dir datasets rbar 1 gnn.layers_mp 17 optim.max_epoch 300 gnn.dim_inner 64 tensorboard_each_run False train.mode my_custom
+python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
