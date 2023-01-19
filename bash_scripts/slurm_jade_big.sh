25dd68064d1c626e6e0a9f3c59395d16f11aa615
BenGutteridge <benpgutteridge@gmail.com>
2023-01-19 13:35:32 +0000

    runs

--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -25,7 +25,7 @@ file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 dir=datasets
 d=64
 L=$SLURM_ARRAY_TASK_ID
-# rbar=1
+rbar=1
 # rbar=$(($SLURM_ARRAY_TASK_ID/2))
 echo "r*=$rbar"
 python3.9 main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
