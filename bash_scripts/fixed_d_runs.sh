cf2fd99e34fa803ef0b492fc5c0e45f6b4bc0802
BenGutteridge <benpgutteridge@gmail.com>
2023-01-19 10:02:40 +0000

    runs

--- bash_scripts/fixed_d_runs.sh
+++ bash_scripts/fixed_d_runs.sh
@@ -7,13 +7,13 @@ task=struct
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
 file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 
-# dir=datasets
-dir="/data/beng/datasets"
+dir=datasets
+# dir="/data/beng/datasets"
 d=64
 L=$1
 rbar=$((L/2))
 echo "r*=$rbar"
 
 python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
-python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar -1 gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+python main.py --cfg "$file" --repeat 3 device cu_d da dataset.dir "$dir" rbar -1 gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
 python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar 1 gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
