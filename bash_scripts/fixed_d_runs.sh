be8df3285b3badb7fa171f1274cb0aa5e0da5463
BenGutteridge <benpgutteridge@gmail.com>
2023-01-20 13:14:48 +0000

    runs

--- bash_scripts/fixed_d_runs.sh
+++ bash_scripts/fixed_d_runs.sh
@@ -15,4 +15,4 @@ L=$1
 # rbar=$((L/2))
 # echo "r*=$rbar"
 
-python main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir "results/no_batchnorm" device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+python main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir "results/no_batchnorm" device cuda dataset.dir "$dir" gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
