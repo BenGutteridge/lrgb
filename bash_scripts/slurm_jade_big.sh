1a89c21214554bb07b840ba6076ef1c2f9d51261
BenGutteridge <benpgutteridge@gmail.com>
2023-01-20 12:56:26 +0000

    runs

--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=NOBNrinf
+#SBATCH --job-name=NOBNr1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -26,7 +26,7 @@ out_dir="results/no_batchnorm"
 dim=64
 L=$SLURM_ARRAY_TASK_ID
 
-# rbar=1
-# python3.9 main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run False train.mode my_custom
-rbar=-1
+rbar=1
 python3.9 main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run False train.mode my_custom
+# rbar=-1
+# python3.9 main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run False train.mode my_custom
