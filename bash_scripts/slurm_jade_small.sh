3b95cbb7ac4df8d8953387b01f7caddf5427d9dc
BenGutteridge <benpgutteridge@gmail.com>
2023-01-20 12:50:53 +0000

    runs

--- bash_scripts/slurm_jade_small.sh
+++ bash_scripts/slurm_jade_small.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#SBATCH --job-name=F.d=64rL/2
+#SBATCH --job-name=NOBNr1,inf
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=30:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=small
 # must be on htc, only one w/ GPUs
 # set number of GPUs
@@ -14,19 +14,19 @@ module load python/anaconda3
 source $condaDotFile
 conda activate lrgb2
 nvcc --version
-python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 pe=none
 task='func'
-# task='struct'
-rbar=1
 # file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
 file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 
 dir=datasets
-d=64
+out_dir="results/no_batchnorm"
+dim=64
 L=$SLURM_ARRAY_TASK_ID
-# rbar=-1
-rbar=$(($SLURM_ARRAY_TASK_ID/2))
-echo "r*=$rbar"
-python main.py --cfg "$file" --repeat 3 device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+
+rbar=1
+python3.9 main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run False train.mode my_custom
+rbar=-1
+python3.9 main.py --cfg "$file" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run False train.mode my_custom
