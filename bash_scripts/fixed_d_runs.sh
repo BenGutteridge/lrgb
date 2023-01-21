702bb30259acc0af252c2fc2d11bfd56d3c07c2c
BenGutteridge <benpgutteridge@gmail.com>
2023-01-21 23:36:42 +0000

    runs

--- bash_scripts/fixed_d_runs.sh
+++ bash_scripts/fixed_d_runs.sh
@@ -2,16 +2,17 @@
 cd ..
 pe=none
 task=func
+rbar=$1
 # task=struct
 # file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
-# file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
+file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 
-residual=$1
-files=(
-  "configs/GCN/peptides-${task}-GCN+${pe}.yaml"
-  "configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
-)
+# residual=$1
+# files=(
+#   "configs/GCN/peptides-${task}-GCN+${pe}.yaml"
+#   "configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
+# )
 
 # dir=datasets
 dir="/data/beng/datasets"
@@ -21,4 +22,7 @@ L=$2
 # rbar=$((L/2))
 # echo "r*=$rbar"
 
-python main.py --cfg "${files[$residual]}" --repeat 3 gnn.batchnorm False gnn.l2norm False out_dir "results/no_batchnorm" device cuda dataset.dir "$dir" gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+python main.py --cfg $file --repeat 3 out_dir "results/no_batchnorm" device cuda dataset.dir "$dir" gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+
+python main.py --cfg "${files[$residual]}" --repeat 3 out_dir "results/no_batchnorm" device cuda dataset.dir "$dir" gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $d tensorboard_each_run False train.mode my_custom
+# gnn.batchnorm False gnn.l2norm False
--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=BNresgcn64
+#SBATCH --job-name=BNr164
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
