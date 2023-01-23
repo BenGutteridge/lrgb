83319f353a5b63e3e251bd1d3a0fb4397f01e9a2
BenGutteridge <benpgutteridge@gmail.com>
2023-01-23 10:35:14 +0000

    uploading runs

--- bash_scripts/slurm_jade_big_500k_gcn.sh
+++ bash_scripts/slurm_jade_big_500k_gcn.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=F500res
+#SBATCH --job-name=F500gcn
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -18,8 +18,8 @@ python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_availa
 pe=none
 task='func'
 # task='struct'
-# file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
-file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
+file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
+# file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
 
 dir=datasets
 # d=64
