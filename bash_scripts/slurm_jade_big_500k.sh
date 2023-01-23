59fa462f40c1c0a3e31ae973f7b896807a0f22e8
BenGutteridge <benpgutteridge@gmail.com>
2023-01-23 10:40:55 +0000

    uploading runs

--- bash_scripts/slurm_jade_big_500k.sh
+++ bash_scripts/slurm_jade_big_500k.sh
@@ -15,12 +15,12 @@ source $condaDotFile
 conda activate lrgb2
 nvcc --version
 python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-pe=none
-# task='func'
-task='struct'
-file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
+pe=LapPE
+task='func'
+# task='struct'
+# file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
-# file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
+file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 
 dir=datasets
 # d=64
