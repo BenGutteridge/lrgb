883988dbff028ee4ddb4417e54c9a7731c8c3c6e
BenGutteridge <benpgutteridge@gmail.com>
2023-01-23 08:46:51 +0000

    uploading runs

--- bash_scripts/pept_func_runs_gcn.sh
+++ bash_scripts/pept_func_runs_gcn.sh
@@ -5,8 +5,8 @@ file="configs/GCN/peptides-func-GCN+${pe}.yaml"
 dir="datasets"
 # dir="/data/beng/datasets"
 train_mode=my_custom
-# stage_type=my_stack
-stage_type=stack_residual
+stage_type=my_stack
+# stage_type=stack_residual
 
 SLURM_ARRAY_TASK_ID=$2
 
