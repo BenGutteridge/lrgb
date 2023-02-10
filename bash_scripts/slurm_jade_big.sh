793c55cb6e33843c12fb4891c8bb2ea841769cda
BenGutteridge <benpgutteridge@gmail.com>
2023-02-10 11:58:35 +0000

    uploading runs

--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=voclapr1
+#SBATCH --job-name=voclaprinf
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -36,7 +36,7 @@ layer=my_gcnconv
 dir=datasets
 out_dir="results"
 L=$SLURM_ARRAY_TASK_ID
-rbar=1
+rbar=-1
 
 # python3.9 main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" rbar $rbar gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
 
