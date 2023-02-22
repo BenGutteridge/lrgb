5668c0f5a78c9a76e1aa57f16373444b65dbac1c
BenGutteridge <benpgutteridge@gmail.com>
2023-02-22 17:36:19 +0000

    uploading runs

--- bash_scripts/slurm_jade_big.sh
+++ bash_scripts/slurm_jade_big.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=kmaxL7nuinf
+#SBATCH --job-name=rho1L23nu1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -45,11 +45,10 @@ layer=my_gcnconv
 dir=datasets
 out_dir="results"
 # L=$SLURM_ARRAY_TASK_ID
-L=7
-nu=-1
-# rho=5
+L=23
+nu=1
 # rho=$SLURM_ARRAY_TASK_ID
-rho=0
+rho=1
 jk=none
 k_max=$SLURM_ARRAY_TASK_ID
 
