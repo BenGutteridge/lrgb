ad7f8d1196c1042db18a847df47d6c7d0f6249bd
BenGutteridge <benpgutteridge@gmail.com>
2023-02-25 12:12:51 +0000

    uploading runs

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=VocJK
+#SBATCH --job-name=pc_nu1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
@@ -19,7 +19,8 @@ source activate $DATA/longrange
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 
-# pe=LapPE
+
+# pe=none
 # task='func'
 # file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
@@ -27,11 +28,11 @@ python -c "import torch; print(torch.__version__); print(torch.cuda.is_available
 
 # file='configs/GCN/vocsuperpixels-GCN.yaml'
 # file='configs/DelayGCN/vocsuperpixels-DelayGCN.yaml'
-file='configs/DelayGCN/vocsuperpixels-DelayGCN+LapPE.yaml'
+# file='configs/DelayGCN/vocsuperpixels-DelayGCN+LapPE.yaml'
 
 # file='configs/GCN/pcqm-contact-GCN+none.yaml'
 # file='configs/GCN/pcqm-contact-GCN+RWSE.yaml'
-# file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
+file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+RWSE.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
 
@@ -48,14 +49,19 @@ layer=my_gcnconv
 dir=datasets
 out_dir="results"
 L=$SLURM_ARRAY_TASK_ID
+# L=23
 nu=1
-rho=5
-jk_mode=$1
+# rho=$SLURM_ARRAY_TASK_ID
+rho=0
+jk=none
+k_max=1000000 # default 1e6
+ckpt_period=10
 
-python main.py --cfg "$file" --repeat 3 jk_mode $jk_mode fixed_params.N 500_000 rho $rho gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
+
+python main.py --cfg "$file" --repeat 3 k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
 
 # FOR NO BN
-# python3.9 main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
+# python main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
 
 # FOR STANDARD BASELINES
-# python3.9 main.py --cfg "$file" --repeat 3 out_dir $out_dir device cuda dataset.dir "$dir" optim.max_epoch 300 tensorboard_each_run True
+# python main.py --cfg "$file" --repeat 3 out_dir $out_dir device cuda dataset.dir "$dir" optim.max_epoch 300 tensorboard_each_run True
