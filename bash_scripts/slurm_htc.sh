7a0bf6419f2c7a29831e38b4a39df9a6c1212d59
BenGutteridge <benpgutteridge@gmail.com>
2023-03-02 11:30:46 +0000

    uploading runs

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=VGateRepro
+#SBATCH --job-name=V1DGGnoCC
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
@@ -10,7 +10,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --account=engs-oxnsg
 
-cd $DATA/repos/clean_lrgb/lrgb
+cd $DATA/repos/lrgb
 
 module purge
 module load Anaconda3
@@ -43,29 +43,40 @@ python -c "import torch; print(torch.__version__); print(torch.cuda.is_available
 # file='configs/SAN/pcqm-contact-SAN.yaml'
 # file='configs/GatedGCN/pcqm-contact-GatedGCN.yaml'
 
+# # DRewGated, VOC 
+file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
+# file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN+LapPE.yaml'
+
+# Just for runing pure SAN
+# python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
+
 # layer=gcnconv
 # layer=my_gcnconv
+layer=drewgatedgcnconv
 
-# dir=datasets
-# out_dir="results"
-# L=$SLURM_ARRAY_TASK_ID
-# L=23
-# nu=1
+dir=datasets
+out_dir=results/no_CC
+L=$SLURM_ARRAY_TASK_ID
+L=8
+nu=1
 # rho=$SLURM_ARRAY_TASK_ID
-# rho=0
-# jk=none
-# k_max=1000000 # default 1e6
-# ckpt_period=10
+rho=0
+jk=none
+k_max=1000000 # default 1e6
+ckpt_period=10
 
-python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
+gnn=drew_gated_gnn
+# gnn=gnn
 
-# python main.py --cfg "$file" --repeat 3 k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
+python main.py --cfg "$file" --repeat 3  model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
 
 # FOR NO BN
 # python main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
 
 # FOR STANDARD BASELINES
 # python main.py --cfg "$file" --repeat 3 out_dir $out_dir device cuda dataset.dir "$dir" optim.max_epoch 300 tensorboard_each_run True
+
+### For ASC
 
 # cfg=configs/rbar-GCN/peptides-struct-DelayGCN+none.yaml
 # A=True
