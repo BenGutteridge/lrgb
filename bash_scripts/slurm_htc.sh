3aa26f7540fddaa3f5d45761526f820cd94d8c52
BenGutteridge <benpgutteridge@gmail.com>
2023-03-04 17:43:50 +0000

    uploading runs

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=VSANRepro
+#SBATCH --job-name=V
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
@@ -10,8 +10,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --account=engs-oxnsg
 
-# cd $DATA/repos/lrgb
-cd $DATA/repos/clean_lrgb/lrgb
+cd $DATA/repos/lrgb
+# cd $DATA/repos/clean_lrgb/lrgb
 
 module purge
 module load Anaconda3
@@ -21,11 +21,11 @@ nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 
 
-# pe=none
-# task='func'
+pe=none
+task='func'
 # file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
 # file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
-# file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
+file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
 
 # file='configs/GCN/vocsuperpixels-GCN.yaml'
 # file='configs/DelayGCN/vocsuperpixels-DelayGCN.yaml'
@@ -46,35 +46,35 @@ python -c "import torch; print(torch.__version__); print(torch.cuda.is_available
 
 # # DRewGated, VOC 
 # file='configs/GatedGCN/vocsuperpixels-GatedGCN.yaml'
-file='configs/GatedGCN/vocsuperpixels-GatedGCN+LapPE.yaml'
+# file='configs/GatedGCN/vocsuperpixels-GatedGCN+LapPE.yaml'
 # file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
 # file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN+LapPE.yaml'
 
 # Just for runing pure SAN
-python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
+# python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
 
 # layer=gcnconv
-# layer=my_gcnconv
+layer=my_gcnconv
 # layer=share_drewgatedgcnconv
-layer=gatedgcnconv_noedge
+# layer=gatedgcnconv_noedge
 
 dir=datasets
-out_dir=results/VOCnoedge
+out_dir=results/core+wavefront
 L=$SLURM_ARRAY_TASK_ID
-L=8
-nu=1
+nu=-1
 # rho=$SLURM_ARRAY_TASK_ID
-rho=0
+rho=1
+rho_max=1000000
 jk=none
-k_max=1000000 # default 1e6
-ckpt_period=5
-edge_encoder=False
+k_max=5 # default 1e6
+ckpt_period=10
+# edge_encoder=False
 
 # gnn=drew_gated_gnn
-gnn=my_custom_gnn
-# gnn=gnn
+# gnn=my_custom_gnn
+gnn=gnn
 
-# python main.py --cfg "$file" --repeat 3  dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
+python main.py --cfg "$file" --repeat 3  dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho rho_max $rho_max train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom
 
 # FOR NO BN
 # python main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
