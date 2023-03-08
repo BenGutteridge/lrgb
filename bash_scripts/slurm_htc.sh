67af9ac10fbd2dcd89a8d362f076bf74b144d961
BenGutteridge <benpgutteridge@gmail.com>
2023-03-08 13:51:44 +0000

    runs

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=PC20lapseeds
+#SBATCH --job-name=V30paper
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -35,7 +35,7 @@ task=struct
 # file='configs/GCN/pcqm-contact-GCN+RWSE.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+RWSE.yaml'
-file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
+# file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
 
 # file='configs/DelayGCN/cocosuperpixels-DelayGCN.yaml'
 # file='configs/DelayGCN/cocosuperpixels-DelayGCN+LapPE.yaml'
@@ -47,23 +47,23 @@ file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
 # # DRewGated, VOC 
 # file='configs/GatedGCN/vocsuperpixels-GatedGCN.yaml'
 # file='configs/GatedGCN/vocsuperpixels-GatedGCN+LapPE.yaml'
-# file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
+file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
 # file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN+LapPE.yaml'
 
 # Just for runing pure SAN
 # python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
 
 # layer=gcnconv
-layer=my_gcnconv
-# layer=share_drewgatedgcnconv
+# layer=my_gcnconv
+layer=share_drewgatedgcnconv
 # layer=drewgatedgcnconv
 # layer=gatedgcnconv_noedge
 
-seed=$SLURM_ARRAY_TASK_ID
+seed=0
 dir=datasets
-out_dir=results/PC_Lap/seed=$seed
-L=20
-nu=-1
+out_dir=results/Vslic30
+L=8
+nu=1
 # rho=$SLURM_ARRAY_TASK_ID
 rho=0
 rho_max=1000000
@@ -71,14 +71,16 @@ jk=none
 k_max=1000000 # default 1e6
 ckpt_period=10
 edge_encoder=False
-epochs=150
+epochs=300
 
-# gnn=drew_gated_gnn
+slic=30
+
+gnn=drew_gated_gnn
 # gnn=alpha_gated_gnn
 # gnn=my_custom_gnn
-gnn=gnn
+# gnn=gnn
 
-python main.py --cfg "$file" --repeat 1  seed $seed dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho rho_max $rho_max train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch $epochs tensorboard_each_run True train.mode my_custom
+python main.py --cfg "$file" --repeat 1  seed $seed dataset.slic_compactness $slic dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho rho_max $rho_max train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch $epochs tensorboard_each_run True train.mode my_custom
 
 # FOR NO BN
 # python main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
