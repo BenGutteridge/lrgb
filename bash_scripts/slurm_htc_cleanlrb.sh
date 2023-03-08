746969af9a5ee985ef83ea2b5fe4babc8ef1831e
BenGutteridge <benpgutteridge@gmail.com>
2023-03-08 14:40:08 +0000

    runs

---
+++ bash_scripts/slurm_htc_cleanlrb.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+#SBATCH --job-name=SANslic30
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=12:00:00
+#SBATCH --partition=short
+# must be on htc, only one w/ GPUs
+#SBATCH --clusters=htc
+# set number of GPUs
+#SBATCH --gres=gpu:1
+#SBATCH --account=engs-oxnsg
+
+# cd $DATA/repos/lrgb
+cd $DATA/repos/clean_lrgb/lrgb
+
+module purge
+module load Anaconda3
+module load CUDAcore/11.1.1
+source activate $DATA/longrange
+nvcc --version
+python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+
+slic=30
+# Just for runing pure SAN
+python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 dataset.slic_compactness $slic tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retrySANslic30
--- run_main.py
+++ run_main.py
@@ -203,7 +203,7 @@ def parse_args() -> argparse.Namespace:
         # 'train.ckpt_period 1',
         # 'alt_postlayer.bn True',
         # 'alt_postlayer.act True',
-        'dataset.slic_compactness 10',
+        'dataset.slic_compactness 30',
         ]
 
     # argpath='results/pept-func_delay_gnn_kmax=03_rho=03_rho_max=10_d=080_L=14/config.yaml'
