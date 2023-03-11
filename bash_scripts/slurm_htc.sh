b997f7537be8fb990d05085c724916296dc1c5d7
BenGutteridge <benpgutteridge@gmail.com>
2023-03-11 17:45:07 +0000

    upload run

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=V30papSE
+#SBATCH --job-name=PCCC
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -33,7 +33,7 @@ task=struct
 
 # file='configs/GCN/pcqm-contact-GCN+none.yaml'
 # file='configs/GCN/pcqm-contact-GCN+RWSE.yaml'
-# file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
+file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+RWSE.yaml'
 # file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
 
@@ -47,23 +47,23 @@ task=struct
 # # DRewGated, VOC 
 # file='configs/GatedGCN/vocsuperpixels-GatedGCN.yaml'
 # file='configs/GatedGCN/vocsuperpixels-GatedGCN+LapPE.yaml'
-file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
+# file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
 # file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN+LapPE.yaml'
 
 # Just for runing pure SAN
 # python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
 
 # layer=gcnconv
-# layer=my_gcnconv
-layer=share_drewgatedgcnconv
+layer=my_gcnconv
+# layer=share_drewgatedgcnconv
 # layer=drewgatedgcnconv
 # layer=gatedgcnconv
 
 seed=$SLURM_ARRAY_TASK_ID
 dir=datasets
-out_dir=results/Vslic30/seed=$seed
-L=8
-nu=1
+out_dir=results/pc_cc/seed=$seed
+L=20
+nu=-1
 # rho=$SLURM_ARRAY_TASK_ID
 rho=0
 rho_max=1000000
@@ -71,14 +71,14 @@ jk=none
 k_max=1000000 # default 1e6
 ckpt_period=10
 edge_encoder=False
-epochs=300
+epochs=80
 
-slic=30
+slic=10
 
-gnn=drew_gated_gnn
+# gnn=drew_gated_gnn
 # gnn=alpha_gated_gnn
 # gnn=my_custom_gnn
-# gnn=gnn
+gnn=gnn
 # gnn=custom_gnn
 
 python main.py --cfg "$file" --repeat 1 seed $seed dataset.slic_compactness $slic dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho rho_max $rho_max train.auto_resume True train.ckpt_period $ckpt_period gnn.layer_type $layer out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch $epochs tensorboard_each_run True train.mode my_custom
