3cb55c0570a01f5f52f3318a7e715dfb7130bd95
BenGutteridge <benpgutteridge@gmail.com>
2023-03-16 20:36:23 +0000

    DRew-GIN

--- bash_scripts/slurm_htc.sh
+++ bash_scripts/slurm_htc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=VDGIN
+#SBATCH --job-name=VDGINnobn
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -94,7 +94,7 @@ slic=30
 gnn=drew_gin
 # gnn=gnn
 # gnn=custom_gnn
-bn=True
+bn=False
 
 nu=1
 L=8
