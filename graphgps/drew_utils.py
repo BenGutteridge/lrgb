ca9a4ea8a3fccb9ee1dba93912a7b0ae8e858fbd
BenGutteridge <benpgutteridge@gmail.com>
2023-03-16 16:01:00 +0000

    changeable alpha in digl

--- graphgps/drew_utils.py
+++ graphgps/drew_utils.py
@@ -75,7 +75,7 @@ def get_run_name(cfg_fname, default):
   if cfg.agg_weights.convex_combo:
     model += '_CC'
   if cfg.dataset.transform != 'none':
-    model += '_%s' % cfg.dataset.transform
+    model += '_%s_alpha=p%02d' % (cfg.dataset.transform, cfg.digl.alpha*100)
   if cfg.spn.K != 0:
     model += '_K=%02d' % cfg.spn.K
   if 'alt_postlayer' in cfg.gnn.stage_type:
