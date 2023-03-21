49d048bca7b4ae43b2e15456fedd03b8f1edf962
BenGutteridge <benpgutteridge@gmail.com>
2023-03-21 17:29:44 +0000

    runs

--- gridsearch_mixhop_pept.py
+++ gridsearch_mixhop_pept.py
@@ -172,8 +172,8 @@ if __name__ == '__main__':
     # Load cmd line args
     for i in range(num_runs):
         print(i)
-        if True:
-        # try:
+        # if True:
+        try:
             args = parse_args()
             # Load config file
             set_cfg(cfg)
@@ -230,5 +230,5 @@ if __name__ == '__main__':
             if args.mark_done:
                 os.rename(args.cfg_file, '{}_done'.format(args.cfg_file))
             logging.info(f"[*] All done: {datetime.datetime.now()}")
-        # except: write_to_file(os.path.join(cfg.run_dir, 'search.txt'), 'FAILED', i)
-        else: pass
+        except: write_to_file(os.path.join(cfg.run_dir, 'search.txt'), 'FAILED', i)
+        # else: pass
