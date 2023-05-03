c05af5c3247bd56b91142f05b50d82d6790172ee
BenGutteridge <benpgutteridge@gmail.com>
2023-05-03 17:50:11 +0100

    exps

--- bash_scripts/slurm_htc_mixhop_pcqm.sh
+++ bash_scripts/slurm_htc_mixhop_pcqm.sh
@@ -21,100 +21,24 @@ nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 
 
-# L=$1
-# nu=$2
-# pe=$3
-# task=$4
-
-# file="configs/GCN/peptides-${task}-GCN+${pe}.yaml"
-# file="configs/GCN/peptides-${task}-ResGCN+${pe}.yaml"
-# file="configs/rbar-GCN/peptides-${task}-DelayGCN+${pe}.yaml"
-
-# file='configs/GCN/vocsuperpixels-GCN.yaml'
-# file='configs/DelayGCN/vocsuperpixels-DelayGCN.yaml'
-# file='configs/DelayGCN/vocsuperpixels-DelayGCN+LapPE.yaml'
-
-# file='configs/GCN/pcqm-contact-GCN+none.yaml'
-# file='configs/GCN/pcqm-contact-GCN+RWSE.yaml'
-# file='configs/DelayGCN/pcqm-contact-DelayGCN+none.yaml'
-# file='configs/DelayGCN/pcqm-contact-DelayGCN+RWSE.yaml'
-# file='configs/DelayGCN/pcqm-contact-DelayGCN+LapPE.yaml'
-
-# file='configs/DelayGCN/cocosuperpixels-DelayGCN.yaml'
-# file='configs/DelayGCN/cocosuperpixels-DelayGCN+LapPE.yaml'
-
 # file='configs/GCN/pcqm-contact-GCN.yaml'
 file='configs/GCN/pcqm-contact-GCN+LapPE.yaml'
-# file='configs/SAN/pcqm-contact-SAN.yaml'
-# file='configs/GatedGCN/pcqm-contact-GatedGCN.yaml'
-# file='configs/GatedGCN/pcqm-contact-GatedGCN+LapPE.yaml'
 
-# # DRewGated, VOC 
-# file='configs/GatedGCN/vocsuperpixels-GatedGCN.yaml'
-# file='configs/GatedGCN/vocsuperpixels-GatedGCN+LapPE.yaml'
-# file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN.yaml'
-# file='configs/DRewGatedGCN/vocsuperpixels-DRewGatedGCN+LapPE.yaml'
-
-# Just for runing pure SAN
-# python main.py --cfg configs/SAN/vocsuperpixels-SAN.yaml --repeat 3 tensorboard_each_run True dataset.dir ../../lrgb/datasets wandb.use False train.ckpt_period 5 device cuda train.auto_resume True out_dir results/retry
-
-# file="configs/DRewGatedGCN/peptides-${task}-DRewGatedGCN+${pe}.yaml"
-
-# layer=gcnconv
-# layer=my_gcnconv
-# layer=share_drewgatedgcnconv
-# layer=drewgatedgcnconv
-# layer=gatedgcnconv_noedge
-
-# seed=$SLURM_ARRAY_TASK_ID
-seed=0
+seed=$SLURM_ARRAY_TASK_ID
+# seed=0
 dir=datasets
-out_dir=results/drewgated_pcqm
-# rho=$SLURM_ARRAY_TASK_ID
-rho=0
-rho_max=1000000
-jk=none
-k_max=1000000 # default 1e6
+out_dir=results/mixhop_pcqm
 ckpt_period=10
 edge_encoder=False
 epochs=90
 use_CC=True
-# digl_alpha=$1
-digl_alpha=0.20
 
-# avg_deg=$SLURM_ARRAY_TASK_ID
-# avg_deg=15
-# tf="digl=$avg_deg"
-tf=none
-
-# gnn=drew_gated_gnn
-# gnn=alpha_gated_gnn
-# gnn=my_custom_gnn
-# gnn=drew_gin
 gnn=mixhop_gcn
-# gnn=gnn
-# gnn=custom_gnn
+
 bn=True
+
 P=3
 
-nu=1
-L=$SLURM_ARRAY_TASK_ID
+# L=$SLURM_ARRAY_TASK_ID
 
-python main.py --cfg "$file" --repeat 1 mixhop_args.max_P $P digl.alpha $digl_alpha dataset.transform $tf seed $seed agg_weights.convex_combo $use_CC dataset.edge_encoder $edge_encoder model.type $gnn k_max $k_max jk_mode $jk fixed_params.N 500_000 rho $rho rho_max $rho_max train.auto_resume True train.ckpt_period $ckpt_period out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch $epochs tensorboard_each_run True train.mode custom
-
-# FOR NO BN
-# python main.py --cfg "$file" --repeat 3 gnn.layer_type $layer gnn.batchnorm False gnn.l2norm False out_dir $out_dir device cuda dataset.dir "$dir" nu $nu gnn.layers_mp $L optim.max_epoch 300 gnn.dim_inner $dim tensorboard_each_run True train.mode my_custom
-
-# FOR STANDARD BASELINES
-# python main.py --cfg "$file" --repeat 3 out_dir $out_dir device cuda dataset.dir "$dir" optim.max_epoch 300 tensorboard_each_run True
-
-### For ASC
-
-# cfg=configs/rbar-GCN/peptides-struct-DelayGCN+none.yaml
-# A=True
-# S=delay_share_gnn
-# C=True
-# out_dir=C
-# nu=-1
-# L=5
-# python main.py --cfg $cfg --repeat 3 gnn.stage_type $S agg_weights.use $A agg_weights.convex_combo $C fixed_params.N 500_000 gnn.layer_type my_gcnconv out_dir "results/$out_dir" device cuda dataset.dir datasets nu $nu gnn.layers_mp $L optim.max_epoch 300 tensorboard_each_run True train.mode my_custom train.auto_resume True train.ckpt_period 10
+python main.py --cfg "$file" --repeat 1 mixhop_args.max_P $P seed $seed agg_weights.convex_combo $use_CC dataset.edge_encoder $edge_encoder model.type $gnn fixed_params.N 500_000 train.auto_resume True train.ckpt_period $ckpt_period out_dir $out_dir device cuda dataset.dir "$dir" optim.max_epoch $epochs tensorboard_each_run True train.mode custom
